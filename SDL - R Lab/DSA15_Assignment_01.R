d8d0a17337b8922a3a987dd3183c7ecae7c9ba44
Niranjan Vinod Patil <30333869+NiranjanVpatil@users.noreply.github.com>
2019-06-22 20:41:46 +0530

    Create DSA15_Assignment_01.R

---
+++ SDL - R Lab/DSA15_Assignment_01.R
@@ -0,0 +1,28 @@
+Name: Niranjan Vinod Patil.
+Batch: DA Lab-1.		
+Roll No: SCETTYDSA15.
+Course: Descriptive Analytics – SDL. 
+----------------------------------------------------------------------------------------------------
+Assignment No: 01
+Installing and loading R packages, set/get working directory.
+Solution:
+getwd()                                      # get Work Directory
+install.packages("dplyr")          # Download Package
+library(dplyr)                           # Load Package
+
+# For Remove Packages use  remove.packages("package name")
+
+install.packages("gapminder")
+library(gapminder)
+
+install.packages("openintro")
+library(openintro)
+
+install.packages("ggplot2")
+library(ggplot2)                     # ggplot2 package is Grammer of Graphics.
+
+install.packages("reader")
+library(reader)                       # reader package is used for to read file.
+
+install.packages("stringr")
+library(stringr)
